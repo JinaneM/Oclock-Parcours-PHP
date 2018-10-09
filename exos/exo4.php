b086bc9b9d528f7c3aa264bbccf309d51564fdcb
benoclock <benjamin@oclock.io>
2018-10-09 11:44:46 +0200

    correction

---
+++ exos/exo4.php
@@ -0,0 +1,71 @@
+<?php
+require_once '../inc/functions.php';
+
+/*
+ * Exo 4 : Luigi To The Rescue
+ *
+ * Luigi: Hi!
+ * It seems like Mario is in trouble. I'm here to help!
+ *
+ * But, you know, Mario is my brother. I don't think we should have
+ * a Mario class, and a Luigi class. Maybe, we can just pass our firstname
+ * when we create the object?
+ *
+ * Oh, and I saw this is not programmed yet, but I want to say my punchline!
+ *
+ * For example, we should be able to use the class this way:
+ *      $mario = new Hero('Mario');
+ *      echo $mario->hello(); // Display: "It's me, Mario!"
+ *      $mario->takeHit();
+ *      $mario->up();
+ *      $mario->takeHit();
+ *      echo $mario->getLives(); // Display: 2
+ *
+ *      $luigi = new Hero('Luigi');
+ *      echo $luigi->hello(); // Display: "It's me, Luigi!"
+ *      $luigi->up();
+ *      $luigi->up();
+ *      $luigi->takeHit();
+ *      echo $luigi->getLives(); // Display: 4
+ */
+
+
+class Hero {
+    private $lives;
+    public $name;
+
+    public function __construct($name) {
+        $this->lives = 3;
+        $this->name = $name;
+    }
+
+    public function getLives() {
+        return $this->lives;
+    }
+
+    public function takeHit() {
+        $this->lives--;
+    }
+
+    public function up() {
+        $this->lives++;
+    }
+
+    public function hello() {
+        return 'It\'s me, '.$this->name.'!';
+    }
+}
+
+
+
+
+
+
+/*
+ * Tests
+ * Pas touche !
+ */
+check(4);
+/*
+$mamie = new Hero('Mamie PHP');
+displayExo(4, method_exists($mamie, 'hello') && $mamie->hello() === "It's me, Mamie PHP!");*/
---
+++ inc/children.php
@@ -0,0 +1,76 @@
+<?php
+
+// Les données sur les petits enfants
+
+$data = [
+    'alphonse' => [
+        'name' => 'Alphonse',
+        'age' => 8,
+        'city' => 'Paris',
+        'mother' => 'Christelle',
+        'father' => 'Jean-Alain'
+    ],
+    'kylian' => [
+        'name' => 'Kylian',
+        'age' => 4,
+        'city' => 'Paris',
+        'mother' => 'Christelle',
+        'father' => 'Jean-Alain'
+    ],
+    'hugo' => [
+        'name' => 'Hugo',
+        'age' => 12,
+        'city' => 'Londres',
+        'mother' => 'Amandine',
+        'father' => 'Juste'
+    ],
+    'kevin' => [
+        'name' => 'Kévin',
+        'age' => 14,
+        'city' => 'Saint-Etienne',
+        'mother' => 'Brenda',
+        'father' => 'John'
+    ],
+    'justine' => [
+        'name' => 'Justine',
+        'age' => 10,
+        'city' => 'Saint-Etienne',
+        'mother' => 'Brenda',
+        'father' => 'John'
+    ],
+    'donald' => [
+        'name' => 'Donald',
+        'age' => 3,
+        'city' => 'Saint-Etienne',
+        'mother' => 'Brenda',
+        'father' => 'John'
+    ],
+    'melissa' => [
+        'name' => 'Mélissa',
+        'age' => 12,
+        'city' => 'Bordeaux',
+        'mother' => 'Cindy',
+        'father' => 'Brendon'
+    ],
+    'astride' => [
+        'name' => 'Astride',
+        'age' => 9,
+        'city' => 'Bordeaux',
+        'mother' => 'Cindy',
+        'father' => 'Brendon'
+    ],
+    'catherine' => [
+        'name' => 'Catherine',
+        'age' => 16,
+        'city' => 'Angers',
+        'mother' => 'Olivia',
+        'father' => 'Olivier'
+    ],
+    'patrick' => [
+        'name' => 'Patrick',
+        'age' => 14,
+        'city' => 'Bordeaux',
+        'mother' => 'Olivia',
+        'father' => 'Olivier'
+    ]
+];
